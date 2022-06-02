e5dfa45e72f873364271eceac8ccbc3361c52a39
JaxRaffnix <75493600+JaxRaffnix@users.noreply.github.com>
2022-06-02 22:02:27 +0200

    works for positive numbers

--- Uebungsblatt4/aufgabe2new.c
+++ Uebungsblatt4/aufgabe2new.c
@@ -1,32 +1,35 @@
 #include <stdio.h>
 
-int print_bit(unsigned int decimal)
+unsigned int print_bit(unsigned int decimal)
 {
-    if (decimal == 0)
+    if (decimal == 0) // return 0 if input is 0
     {
         return 0;
     }
 
-    int remainder[16] = {0};
-    int base = 2;
-    
-    int i = 0;
+    int remainder[16] = {0};        // size of binary: 16 bit
+    int base = 2; // base 2 means binary numbers
+
+    int i = 0; // i is the bit size of the binary number
     while (decimal > 0)
     {
-        remainder[i] = decimal % 2;
-        decimal = decimal / 2;
-        i++;
+        remainder[i] = decimal % 2; // store remainder of division in remainder
+        decimal = decimal / 2;      // take result of division as next value
+        i++;                        // increment i until decimal reaches 0
     }
-    
-    // int binary[16] = {0};
-    // for (int j = i - 1; j >= 0; j--)
+
+    // for (int j = i - 1; j >= 0; j--) // set j to binary size -1. Decrement j until 0 is reached
     // {
-    //     binary[i - 1 - j] = remainder[j]; 
+    //     printf("%d", remainder[j]);     // print each bit of remainder in reverse order
     // }
+    // printf("\n");
 
-    // return binary;
-
-    return remainder;
+    unsigned int binary = 0;     // set output to 0
+    for (int k = i - 1; k >= 0; k--)        // take remainder in reverse order
+    {
+        binary = 10 * binary + remainder[k];        // set value as next place of output
+    }
+    return binary;
 }
 
 int main()
@@ -38,4 +41,5 @@ int main()
     printf("Input: %d\n", n);
 
     printf("Output: %d\n", print_bit(n));
+    // print_bit(n); 
 }
