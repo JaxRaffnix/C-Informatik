253e260293069e9018a17972b75669ead31b4490
Jax Raffnix <75493600+JaxRaffnix@users.noreply.github.com>
2022-10-13 22:06:50 +0200

    finished first task

--- Informatik 2/Uebungsblatt1/Aufgabe1.c
+++ Informatik 2/Uebungsblatt1/Aufgabe1.c
@@ -4,16 +4,18 @@
 int main()
 {
 
-    char string[] = "Zeichenketten", c;                     // create two character variables. First one stores the input, second is temporary string
+    char string[] = "Zeichenketten", hold;                  // create two character variables. First one stores the input, second is temporary string
+    char *pointerToString = string;
+
     int i, j, stringLength;                                 // create counter variables
 
     stringLength = strlen(string);                          // ermittelt die Laenge von buf
 
     for (i = 0, j = stringLength - 1; i < j; i++, j--)      // loop half the string length 
     {
-        c = string[i];
-        string[i] = string[j];
-        string[j] = c;
+        hold = pointerToString[i];  	                                // store current character in hold variable
+        string[i] = pointerToString[j];                              // replace current character with mirrored character
+        string[j] = hold;                                   // replace mirrored character with current character
     }
 
     printf("Output: %s\n", string);
