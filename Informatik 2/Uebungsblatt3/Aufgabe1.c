b10fe866a7f981244b155976f19fad25576b16f4
Jax Raffnix <75493600+JaxRaffnix@users.noreply.github.com>
2022-12-12 16:17:17 +0100

    worked on task 4

--- Informatik 2/Uebungsblatt3/Aufgabe1.c
+++ Informatik 2/Uebungsblatt3/Aufgabe1.c
@@ -1,12 +1,8 @@
 int main()
 {
-    // A1
     struct point (double x, y);         // ; eingefügt
     struct point {double x, double y};
     struct point {double x, double y;};
     struct point {double x, double y} ; // ; eingefügt
     struct point {double x, y};         // richtig
-
-    // A2
-    
 }
--- Informatik 2/Uebungsblatt3/Aufgabe4.c
+++ Informatik 2/Uebungsblatt3/Aufgabe4.c
@@ -10,12 +10,70 @@ struct student {
     };
 };
 
-int read_file (struct student *s, int max)
+int number_of_entries (struct student *s, int max)
 {
+    char filename[20];
+    printf("Specify input file, max. name length is 20 characters:\n");
+    scanf("%s", filename);
+    FILE* inputfile = fopen (filename, "r");
+
+    if (inputfile == NULL)
+    {
+        return 0;
+    }
+    
+    int lines;
+    char temp;
+    while (!feof (inputfile))
+    {
+        temp = fgetc (inputfile);
+        if (temp == '\n')
+        {
+            lines++;
+        }
+    }
+    return lines;
     
 }
 
+unsigned int age (struct student *s)
+{
+    struct current_time
+    {
+        int day;
+        int month;
+        int year;
+    };
+    struct current_time ws2022;
+    ws2022.day = 12;
+    ws2022.month = 12;
+    ws2022.year = 2022;
+
+
+
+};
+
+int youngest (struct student *s, int length)
+{
+    struct youngest {
+        unsigned int age;
+        struct student youngest;
+    };
+    struct youngest findyoungest;
+
+    findyoungest.age = 100;
+    for (int i = 0; i < length; i++)
+    {
+        if (age(mystudentlist[i] < findyoungest.age))
+        {
+            findyoungest.age = age(mystudentlist[i]);
+            findyoungest.youngest = mystudentlist[i];
+        }
+    }
+    return findyoungest;
+};
+
 int main()
 {
-struct student studentlist[100];
+    struct student mystudentlist[100];
 }
